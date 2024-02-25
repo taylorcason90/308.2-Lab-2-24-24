a9ff99c1ee3de45292d3800969afa530f63309fb
taylorcason90 <taylorcason90@gmail.com>
2024-02-24 21:13:11 -0500

    control flow lab

---
+++ controlflowassgnmnt22424.js
@@ -0,0 +1,91 @@
+// EXAMPLE BELOW
+
+// let i = 1;
+// do
+//  {
+//   console.log(i);
+//   i++;
+// }
+// while
+//  (i < 20);
+
+
+// code below prints "Sunday"
+// let day = 2;
+// switch (
+// day
+// ) {
+  
+// case
+//  1:
+// console.log("Saturday");
+//     break;
+  
+// case
+//  2:
+//     console.log("Sunday");
+    
+// break
+// ;
+// }
+
+
+// ASSIGNMENT EXAMPLES
+let day = 1;
+let weeks = 1;
+const radius = 5;
+const PI=3.1415;
+const area = PI * radius * radius;
+const plant = .8;
+let startplantnum20;
+let plantstartareatotal = 25;
+plant 
+let plantcapacitymin = plantstartareatotal / plant;
+let plantsperweek = plantstartareatotal * 2; 
+let plantmax = plantstartareatotal / plant;
+
+
+// objective answers
+
+if (plantsperweek >  plantcapacitymin * .8 ) {
+//console.log("Exceeding Capacity");
+ } else {
+//    console.log("All good go to go!");
+ }
+
+
+//NEW EXAMPLE for MONITORED PART
+
+if (plantsperweek > .5 * plantmax) {
+    console.log("exceeds capactity")
+} else {
+    console.log("Everything is good")
+}
+
+if (plantsperweek < .8 * plantmax) {
+    console.log("exceeds capactity", plantsperweek)
+
+} else {
+    console.log("Everything is good" )
+}
+
+
+//
+if (plantsperweek < .5 * plantmax) {
+    console.log("Space avaiable for more plants", plantsperweek)
+
+} else {
+    console.log("No Space" )
+}
+
+ 
+
+if (false) {
+
+    console.log('True');
+ 
+ } else {
+ 
+    console.log('False');
+ 
+ }
